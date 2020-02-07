a47fcc9f1750bcb837ae4e3eb9377af46b60fe21
NastyGamer <jones.m.987654321@gmail.com>
2020-02-07 20:59:12 +0100

    Some more simple starting code

--- Game/src/main/java/hollowrealm/studios/game/Starter.java
+++ Game/src/main/java/hollowrealm/studios/game/Starter.java
@@ -2,6 +2,7 @@ package hollowrealm.studios.game;
 
 import simple.engine.Engine;
 import simple.engine.modules.FrameListener;
+import simple.engine.modules.MouseWheelListener;
 import simple.engine.util.GameConfig;
 import simple.engine.util.Logger;
 
@@ -10,16 +11,26 @@ import java.util.ArrayList;
 
 public class Starter {
 
+    private static float zoom = 1f;
+
     public static void start(GameConfig config, ArrayList<Plugin> plugins) {
         Engine.initialize(config);
         Logger.disableLevel("fps");
         plugins.forEach(Plugin::start);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> plugins.forEach(Plugin::stop)));
+        Image i = Engine.storageModule.getImage("TreeStomp.png").getSubimage(0, 0, 137, 137);
         Engine.graphicModule.addFrameListener(new FrameListener() {
             @Override
             public void onNextFrame(Graphics2D graphics2D) {
-                graphics2D.drawImage(Engine.storageModule.getImage("TreeStomp.png"), 0, 0, null);
+                graphics2D.scale(zoom, zoom);
+                graphics2D.drawImage(i, 0, 0, null);
             }
         }, 0);
+        Engine.mouseModule.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void onMouseScroll(int i) {
+                zoom += i > 0 ? -0.1f : 0.1f;
+            }
+        });
     }
 }
