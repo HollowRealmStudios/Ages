e3547f0e3d53d57bc6783cb494d465688405f5a1
NastyGamer <jones.m.987654321@gmail.com>
2020-02-12 20:26:15 +0100

    Implemented semi-transparent water

--- Game/src/main/java/hollowrealm/studios/game/Starter.java
+++ Game/src/main/java/hollowrealm/studios/game/Starter.java
@@ -12,8 +12,6 @@ import java.util.ArrayList;
 
 public class Starter {
 
-    private static int i = 0;
-
     public static void start(GameConfig config, ArrayList<Plugin> plugins) {
         Engine.initialize(config);
         Logger.disableLevel("fps");
@@ -21,24 +19,17 @@ public class Starter {
             plugins.forEach(Plugin::start);
             Runtime.getRuntime().addShutdownHook(new Thread(() -> plugins.forEach(Plugin::stop)));
         }
-        BufferedImage[] bench = ImageSplitter.split(Engine.storageModule.getImage("WorkBench.png"));
-        BufferedImage[] birch = ImageSplitter.split(Engine.storageModule.getImage("BirchWood.png"));
+        BufferedImage grass = ImageSplitter.split(Engine.storageModule.getImage("Grass.png"))[0];
+        BufferedImage water = ImageSplitter.split(Engine.storageModule.getImage("Water.png"))[0];
+        BufferedImage bench = ImageSplitter.split(Engine.storageModule.getImage("WorkBench.png"))[0];
         Engine.graphicModule.addFrameListener(new FrameListener() {
             @Override
             public void onNextFrame(Graphics2D graphics2D) {
-                graphics2D.drawImage(bench[i], 0, 0, null);
-                graphics2D.drawImage(birch[i], 129, 0, null);
+                graphics2D.drawImage(grass, 0, 100, null);
+                graphics2D.drawImage(grass, 64, 132, null);
+                graphics2D.drawImage(water, 0, 37, null);
+                graphics2D.drawImage(bench, 64, 69, null);
             }
         }, 0);
-        Engine.keyModule.addKeyListener(KeyEvent.VK_D, () -> {
-            if (i == 3) {
-                i = 0;
-            } else i++;
-        });
-        Engine.keyModule.addKeyListener(KeyEvent.VK_A, () -> {
-            if (i == 0) {
-                i = 3;
-            } else i--;
-        });
     }
 }
