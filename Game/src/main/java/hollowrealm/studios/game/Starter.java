b0fada3ee8136773653c44147eedadf5a101dcbb
NastyGamer <jones.m.987654321@gmail.com>
2020-02-12 19:52:21 +0100

    Implemented simple TileRotation using the A and D keys

--- Game/src/main/java/hollowrealm/studios/game/Starter.java
+++ Game/src/main/java/hollowrealm/studios/game/Starter.java
@@ -1,37 +1,45 @@
 package hollowrealm.studios.game;
 
-import hollowrealm.studios.game.tiles.GrassTile;
-import hollowrealm.studios.game.tiles.TestTile;
-import hollowrealm.studios.game.tiles.TileMap;
 import simple.engine.Engine;
 import simple.engine.modules.FrameListener;
+import simple.engine.util.ColorOut;
 import simple.engine.util.GameConfig;
 import simple.engine.util.Logger;
+import simple.engine.util.OverflowingInteger;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Starter {
 
-    private static final ArrayList<Rhomb> rhombs = new ArrayList<>();
-    private static final TileMap map = new TileMap(10, 10, 10);
+    private static int i = 0;
 
     public static void start(GameConfig config, ArrayList<Plugin> plugins) {
         Engine.initialize(config);
         Logger.disableLevel("fps");
         plugins.forEach(Plugin::start);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> plugins.forEach(Plugin::stop)));
-        BufferedImage[] images = ImageSplitter.split(Engine.storageModule.getImage("Grass.png"));
+        BufferedImage[] images = ImageSplitter.split(Engine.storageModule.getImage("WorkBench.png"));
         Engine.graphicModule.addFrameListener(new FrameListener() {
             @Override
             public void onNextFrame(Graphics2D graphics2D) {
-                graphics2D.drawImage(images[0], 0, 0, null);
-                graphics2D.drawImage(images[0], 0, 130, null);
-                graphics2D.drawImage(images[0], 130, 0, null);
-                graphics2D.drawImage(images[0], 130, 130, null);
-
+                graphics2D.drawImage(images[i], 0, 0, null);
+                ColorOut.print(System.out, String.valueOf(i), ColorOut.CYAN);
             }
         }, 0);
+        Engine.keyModule.addKeyListener(KeyEvent.VK_D, () -> {
+            if(i == 3) {
+                i = 0;
+            }
+            else i++;
+        });
+        Engine.keyModule.addKeyListener(KeyEvent.VK_A, () -> {
+            if(i == 0) {
+                i = 3;
+            }
+            else i--;
+        });
     }
 }
